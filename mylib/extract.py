4787ec8b23b045bd31924b4630ab8d85c6dd4396
ag826 <adilgazder2000@gmail.com>
2024-10-04 06:08:44 -0400

    changing mylib

--- mylib/extract.py
+++ mylib/extract.py
@@ -2,6 +2,7 @@ import requests
 
 
 def extract(
+    url="https://github.com/nogibjj/ag825_sqlite_lab/blob/main/Cancer_Data.csv",
     file_path="Cancer_Data.csv",
 ):
     with requests.get(url) as r:
--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -3,19 +3,21 @@ import csv
 import os
 
 
-# load the csv file and insert into a new sqlite3 database
-def load(dataset="Cancer_Data.csv"):
+def load(
+    dataset=r"C:\Users\asus\Desktop\DUKE\STUDIES\FALL 2024\IDS 706 - Data Engineering Systems\ag825_sqlite_lab\Cancer_Data.csv",
+):
 
     # prints the full working directory and path
     print(os.getcwd())
     payload = csv.reader(open(dataset, newline=""), delimiter=",")
-    conn = sqlite3.connect("CancerDB.db")
-    next(subset_data)
+    conn = sqlite3.connect("Cancer_Data.csv")
     c = conn.cursor()
     c.execute("DROP TABLE IF EXISTS CancerDB")
-    c.execute("CREATE TABLE CancerDB (id,diagnosis)")
+    c.execute(
+        "CREATE TABLE CancerDB (id,diagnosis,radius_mean,texture_mean,perimeter_mean,area_mean,smoothness_mean)"
+    )
     # insert
-    c.executemany("INSERT INTO CancerDB VALUES ( ?, ?)", payload)
+    c.executemany("INSERT INTO CancerDB VALUES ( ?, ?,?,?,?,?,?)", payload)
     conn.commit()
     conn.close()
     return "CancerDB.db"
